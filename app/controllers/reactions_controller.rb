c464cee15fbfb607ad74e2b57af4a5567481a4fd
Tallal Ali <tallalali1@gmail.com>
2022-04-27 12:34:39 +0500

    reactions like option added

--- app/controllers/reactions_controller.rb
+++ app/controllers/reactions_controller.rb
@@ -1,11 +1,21 @@
 class ReactionsController < ApplicationController
    before_action :find_tweet
   def create
-    @tweet.likes.create(user_id: current_user.id)
+    if already_liked?
+      flash[:notice] = "You can't like more than once"
+    else
+      @tweet.reactions.create(user_id: current_user.id)
+    end
     redirect_to tweet_path(@tweet)
   end
   private
   def find_tweet
-    @tweet = Tweet.find(params[:post_id])
+    @tweet = Tweet.find(params[:tweet_id])
   end
+
+  def already_liked?
+    Reaction.where(user_id: current_user.id, tweet_id:
+    params[:tweet_id]).exists?
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,9 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-  resources :tweets
+  resources :tweets do
+    resources :reactions
+  end
 
   resources :comments, only: %i[new create destroy] do
     # resources :reactions, only: %i[create]
