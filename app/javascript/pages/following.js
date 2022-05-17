86a859c96f60130ff4f35f5074a2cb7bf49fb70b
Tallal Ali <tallalali1@gmail.com>
2022-05-17 18:12:37 +0500

    ajax calls for follow unfollow

--- app/javascript/pages/following.js
+++ app/javascript/pages/following.js
@@ -1,33 +1,41 @@
 $(document).on("ready turbolinks:load", function () {
+  let preFollow = $(".follows_ids").data("pre_follow");
+  function showFollowButton(){
 
+    if (preFollow > 0 ){
+     $(".follow-button").removeClass("hidden");
+    } else if (preFollow <= 0 ){
+      $(".unfollow-button").removeClass("hidden");
+    }
 
-    $("#follow-button").on("click", function (e) {
-      console.log(e);
-      e.preventDefault();
-      user = $(".follows_ids").data("id");
-      profile = $(".follows_ids").data("profile_id");
-
-      $.ajax({
-        type: "POST",
-        url: `/users/${profile}/follows.json`,
-        data: {
-          follower_user_id: user,
-        },
-        success: function () {
-          console.log("I WAS CALLED");
-          $("#follow-button").hide();
-          $("#unfollow").show();
-        },
-        error: function () {
-          alert("error");
-          debugger;
-        },
-      });
+  };
+  showFollowButton();
+  $(".follow-button").on("click", function (e) {
+    console.log(e);
+    e.preventDefault();
+    user = $(".follows_ids").data("id");
+    profile = $(".follows_ids").data("profile_id");
+    let followButton = document.querySelector(".follow-button");
+    if (followButton.className == "follow-button") {$.ajax({
+      type: "POST",
+      url: `/users/${profile}/follows.json`,
+      data: {
+        follower_user_id: user,
+      },
+      success: function () {
+        console.log("I WAS CALLED");
+        $(".follow-button").addClass("hidden");
+        $(".unfollow-button").removeClass("hidden");
+      },
+      error: function () {
+        alert("error");
+        debugger;
+      },
     });
-});
+  } 
+  });
 
-$(document).on("ready turbolinks:load", function () {
-  $("#unfollow").on("click", function (e) {
+  $(".unfollow-button").on("click", function (e) {
     e.preventDefault();
     user = $(".follows_ids").data("id");
     profile = $(".follows_ids").data("profile_id");
@@ -35,9 +43,9 @@ $(document).on("ready turbolinks:load", function () {
       $.ajax({
         type: "DELETE",
         url: `/users/${user}/follows/${profile}.json`,
-        success: function() {
-            $("#follow-button").show();
-            $("#unfollow").hide();
+        success: function () {
+        $(".unfollow-button").addClass("hidden");
+        $(".follow-button").removeClass("hidden");
         },
       });
     }
